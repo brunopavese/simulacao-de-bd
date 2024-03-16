525dba6ebb3e3fa24d2d1374ff41ab956c9ca278
Bruno Pavese <bruno@ejcm.com.br>
2024-03-15 21:47:37 -0300

    aplicando um try finally no App principal

--- src/App.ts
+++ src/App.ts
@@ -4,9 +4,10 @@ import * as fs from 'fs'
 
 const bd = BancoDeDados.instancia
 
-const menu = new Menu(bd)
-
-menu.abrir()
-
-const jsonData = JSON.stringify(bd.listar())
-fs.writeFileSync('dados.json', jsonData)
+try {
+  const menu = new Menu(bd)
+  menu.abrir()
+} finally {
+  const jsonData = JSON.stringify(bd.listar())
+  fs.writeFileSync('dados.json', jsonData)
+}
